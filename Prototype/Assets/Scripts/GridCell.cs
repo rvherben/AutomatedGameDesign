6d4d536a8fb2eb4d7efd3c9692ff3eac55c990d5
Robin <robinherben@gmail.com>
2018-06-01 21:53:24 +0200

    finishes prototype

---
+++ Prototype/Assets/Scripts/GridCell.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridCell : MonoBehaviour {
+
+    public string cellName;
+
+    public Transform blockadesHolder;
+    public Transform obstaclesHolder;
+    public Transform resourcesHolder;
+
+    Text _buttonText;
+    List<Image> _backgroundImages;
+
+    List<GameObject> _blockades;
+    List<GameObject> _obstacles;
+    List<GameObject> _resources;
+
+    float cellHeat;
+
+	public void Init (float heat)
+    {
+        _backgroundImages = new List<Image>();
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            if(transform.GetChild(i).name == "Background")
+            {
+                _backgroundImages.Add(transform.GetChild(i).GetComponent<Image>());
+            }
+        }
+        _buttonText = transform.Find("ButtonText").GetComponent<Text>();
+
+        cellHeat = heat;
+
+        _blockades = new List<GameObject>();
+        _obstacles = new List<GameObject>();
+        _resources = new List<GameObject>();
+
+        for(int i = 0; i < blockadesHolder.childCount; i++)
+        {
+            if(blockadesHolder.GetChild(i).name == cellName)
+            {
+                _blockades.Add(blockadesHolder.GetChild(i).gameObject);
+            }
+        }
+
+        for (int i = 0; i < obstaclesHolder.childCount; i++)
+        {
+            if (obstaclesHolder.GetChild(i).name == cellName)
+            {
+                _obstacles.Add(obstaclesHolder.GetChild(i).gameObject);
+            }
+        }
+
+        for (int i = 0; i < resourcesHolder.childCount; i++)
+        {
+            if (resourcesHolder.GetChild(i).name == cellName)
+            {
+                _resources.Add(resourcesHolder.GetChild(i).gameObject);
+            }
+        }
+
+        foreach (GameObject obj in _obstacles)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in _blockades)
+        {
+            obj.SetActive(false);
+        }
+        foreach (GameObject obj in _resources)
+        {
+            obj.SetActive(false);
+        }
+
+    }
+
+    public void UpdateCell()
+    {
+        //Activating obstacles based on how much heat there is
+        foreach(GameObject obstacle in _obstacles)
+        {
+            obstacle.SetActive(false);
+            obstacle.SetActive(Random.Range(0.0f, 100f) < Heat);
+        }
+
+        //Activating blockades based on how much heat there is
+        foreach (GameObject blockade in _blockades)
+        {
+            blockade.SetActive(false);
+            blockade.SetActive(Random.Range(0.0f, 100f) < Heat);
+        }
+
+        //Making sure that not all blockades in a cell are active at the same time
+        if (_blockades.Count > 1)
+        {
+            int activeBlockadeCount=0;
+            foreach(GameObject blockade in _blockades)
+            {
+                if(blockade.activeSelf == true)
+                {
+                    activeBlockadeCount++;
+                }
+            }
+            if(activeBlockadeCount == _blockades.Count)
+            {
+                _blockades[Random.Range(0, _blockades.Count)].SetActive(false);
+            }
+        }
+
+        //Deactivating resources based on how much heat there is
+        foreach (GameObject resource in _resources)
+        {
+            resource.SetActive(false);
+            resource.SetActive(Random.Range(0.0f, 100f) > Heat);
+        }
+
+        _buttonText.text = Mathf.Round(cellHeat).ToString();
+    }
+
+    //Sets cell background and heat text to visible or invisible based on passed boolean
+    public void SetDiagnosticsActive(bool active)
+    {
+        _buttonText.gameObject.SetActive(active);
+        if (active)
+        {
+            foreach(Image image in _backgroundImages)
+            {
+                image.color = new Color(image.color.r, image.color.g, image.color.b, 0.08f);
+            }
+        }
+        else
+        {
+            foreach (Image image in _backgroundImages)
+            {
+                image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+            }
+        }
+    }
+
+    public float Heat
+    {
+        get
+        {
+            return cellHeat;
+        }
+        set
+        {
+            cellHeat = value;
+        }
+    }
+
+
+}
---
+++ Prototype/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public Transform gridCellHolder;
+
+    //All cells
+    List<GridCell> _gridCells;
+
+    //Accumulated heat in the current wave per cell
+    List<float> _cellsHeatThisWave;
+
+    //The heat every cell starts with at the start of the game
+    const float _DEFAULT_STARTING_HEAT_PER_CELL = 20;
+
+    //The rate of which cells heat deteriorate every wave
+    const float _DETERIORATION_RATE = 1f;
+
+    //The rate of which heat increases per kill
+    const float _HEAT_INCREASE_PER_KILL = 5f;
+
+    bool _diagnosticsActive = true;
+
+    void Start ()
+    {
+        _gridCells = new List<GridCell>();
+        _cellsHeatThisWave = new List<float>();
+
+        //Initalization of lists
+        for (int i = 0; i<gridCellHolder.childCount; i++)
+        {
+            _gridCells.Add(gridCellHolder.GetChild(i).GetComponent<GridCell>());
+            _cellsHeatThisWave.Add(0);
+        }
+
+        //Initializing and setting starting heat for each cell
+        foreach(GridCell cell in _gridCells)
+        {
+            cell.Init(_DEFAULT_STARTING_HEAT_PER_CELL);
+            cell.UpdateCell();
+        }
+
+        //Makes sure diagnostics are off at startup
+        HandleDiagnosticsButtonPressed();
+    }
+	
+    //Handler for when a kill occurs within a cell
+	public void HandleKillInCell(int number)
+    {
+        //Updating the accumulated heat this wave for the cell in which the kill occured
+        _cellsHeatThisWave[number] += _HEAT_INCREASE_PER_KILL;
+    }
+
+    //Handler for when the wave is over and the next wave needs to be started
+    public void HandleEndOfWave()
+    {
+        for(int i = 0; i < _gridCells.Count; i++)
+        {
+            //Set new cell heat
+            _gridCells[i].Heat -= _DETERIORATION_RATE;
+            if (_gridCells[i].Heat < 0)
+            {
+                _gridCells[i].Heat = 0;
+            }
+
+            _gridCells[i].Heat += _cellsHeatThisWave[i];
+            if (_gridCells[i].Heat > 100)
+            {
+                _gridCells[i].Heat = 100;
+            }
+
+            //Reset accumulated heat for this wave
+            _cellsHeatThisWave[i] = 0;
+            
+            //Update cells
+            _gridCells[i].UpdateCell();
+        }  
+    }
+
+    //Handles reset button and resets all cells to starting state
+    public void HandleReset()
+    {
+        for(int i = 0; i<_cellsHeatThisWave.Count; i++)
+        {
+            _cellsHeatThisWave[i] = 0;
+            _gridCells[i].Heat = _DEFAULT_STARTING_HEAT_PER_CELL;
+            _gridCells[i].UpdateCell();
+        }
+    }
+
+    //Handles show diagnostics button
+    public void HandleDiagnosticsButtonPressed()
+    {
+        _diagnosticsActive = !_diagnosticsActive;
+
+        foreach (GridCell cell in _gridCells)
+        {
+            cell.SetDiagnosticsActive(_diagnosticsActive);
+        }
+    }
+}
